5a4c3144843b17a4fa44d1942aa82757185e8cdb
B-gooroom <j.sean1316@gmail.com>
2022-05-14 18:47:11 +0900

    feat: 테트리스 블럭 이동(keyEvent) & computed  적용($)

--- src/components/Tetris.svelte
+++ src/components/Tetris.svelte
@@ -1,61 +1,100 @@
-<script>
-  const iBlock = [0, 0];
-  const blocks = [
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-  ];
-  const O = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}];
-  const I = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}];
-  const T = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 5, y: 1}];
-  const J = [{x: 5, y: 0}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 4, y: 2}];
-  const L = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}];
-  const Z = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 5, y: 1}, {x: 6, y: 1}];
-  const S = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}];
+<!-- TODO: 'spaceBar'로 끝까지 내리고, 고정, 다음블럭 부르기(랜덤?)
+TODO: 블럭들 transform
+TODO: x 한줄이 다 차면 없애기(한줄 out) - score +10
+TODO: timer 1000 - blocks y + 1 (한줄씩 내려오기) -->
 
-  const keyEvent = (event) => {
-    switch (event.key) {
-      case "ArrowLeft":
-        // this.directionBar('bar2Direct', -10)
-        console.log("arrowLeft");
-        break;
-      case "ArrowRight":
-        // this.directionBar('bar2Direct', 10)
-        console.log("arrowRight");
-        break;
-      default:
-        break;
-    }
-  };
-  const checkGrid = (x, y) => {
-    let isFill = false;
-    for (let index = 0; index < S.length; index++) {
-      const child = S[index]; 
-      if (child.x === x && child.y === y) {
-        isFill = true;
-        break;
+<script>
+import { onDestroy } from 'svelte';
+
+const blocks = [
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+];
+let O = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}];
+let I = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}];
+let T = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 5, y: 1}];
+let J = [{x: 5, y: 0}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 4, y: 2}];
+let L = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}];
+let Z = [{x: 4, y: 0}, {x: 5, y: 0}, {x: 5, y: 1}, {x: 6, y: 1}];
+let S = [{x: 4, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}];
+
+const keyEvent = (event) => {
+  console.log(event);
+  switch (event.key) {
+    case "ArrowLeft":
+      if (O[0].x === 0) return;
+      for (let index = 0; index < O.length; index++) {
+        const child = O[index];
+        child.x -= 1;
       }
-    }
-    return isFill ? 'fill' : 'empty';
+      O = [...O];
+      break;
+    case "ArrowRight":
+      if (O[0].x === 8) return;
+      for (let index = 0; index < O.length; index++) {
+        const child = O[index];
+        child.x += 1;
+      }
+      O = [...O];
+      console.log("arrowRight");
+      break;
+    case "ArrowDown":
+      if (O[0].y === 19) return;
+      for (let index = 0; index < O.length; index++) {
+        const child = O[index];
+        child.y += 1;
+      }
+      O = [...O];
+      console.log("ArrowDown");
+      break;
+      // TODO: 모양바꾸기(블럭)
+      // case "ArrowUp":
+      // if (O[0].y === 19) return;
+      // for (let index = 0; index < O.length; index++) {
+      //   const child = O[index];
+      //   child.y += 1;
+      // }
+      // O = [...O];
+      // console.log("ArrowUp");
+      // break;
+    default:
+      break;
   }
+};
+$: checkGrid = (x, y) => {
+  let isFill = false;
+  for (let index = 0; index < O.length; index++) {
+    const child = O[index]; 
+    if (child.x === x && child.y === y) {
+      isFill = true;
+      break;
+    }
+  }
+  return isFill ? 'fill' : 'empty';
+}
+
+document.addEventListener('keydown', keyEvent);
+
+onDestroy(() => document.removeEventListener('keydown', keyEvent));
 </script>
 
 <h3>Play Tetris!</h3>
@@ -66,9 +105,7 @@
         <tr>
           <td class="extra" />
           {#each gridY as gridX, x}
-            <!-- <td class={gridX ? "fill" : "empty"}> -->
             <td class={checkGrid(x, y)}>
-              <!-- {gridX} -->
             </td>
           {/each}
           <td class="extra" />
